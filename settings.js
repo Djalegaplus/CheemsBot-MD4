3d22a4ba25ca83d8f7e77b858dc249ca8520dc53
Djalegaplus <113895954+Djalegaplus@users.noreply.github.com>
2022-09-26 19:17:17 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,7 +54,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['7059114934'] //ur owner number
+global.vcardowner = ['+91 70591 14934'] //ur owner number
 global.ownername = "Ajay" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
